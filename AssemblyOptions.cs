1b23e5dae7706b346f1737dd89a6a536b397d317
sukasa <nick.m@shaw.ca>
2026-01-09 19:04:56 -0800

    Move assembly options to new file

---
+++ AssemblyOptions.cs
@@ -0,0 +1,26 @@
+using CommandLine;
+using JetBrains.Annotations;
+
+namespace IC10_Inliner;
+
+public static partial class IC10Assembler
+{
+    [UsedImplicitly]
+    public class AssemblyOptions
+    {
+        [Value(0, Required = true)]
+        public string Filename { get; set; } = "";
+
+        [Option('c', "comments", Default = false)]
+        public bool IncludeComments { get; set; } = false;
+
+        [Option('s', "sections")]
+        public IEnumerable<string>? IncludeSections { get; set; } = [];
+
+        [Option('m', "keep-macros", Default = false)]
+        public bool ElideMacros { get; set; } = false;
+
+        [Option('S', "symbols", Default = false)]
+        public bool EmitSymbolFile { get; set; } = false;
+    }
+}
