c1d82f5c78daba9024f52268cfa00d67be96412e
Tiogshi Laj <tiogshi@gmail.com>
2026-01-15 12:41:39 -0800

    added basic macro support

--- IC10Program.cs
+++ IC10Program.cs
@@ -1,10 +1,45 @@
-﻿using System.Globalization;
+﻿using JetBrains.Annotations;
+using System.Globalization;
 
 namespace IC10_Inliner
 {
+    public class Macro
+    {
+        public string Name { get; private set; }
+        public int SourceLine { get; private set; }
+        public List<string> ParamNames { get; private set; }
+        private List<string> BodyLines = new();
+
+        public Macro(string Name, IEnumerable<string> Params, int sourceLine)
+        {
+            this.Name = Name;
+            ParamNames = Params.ToList();
+            SourceLine = sourceLine;
+        }
+
+        public void Add(string Line)
+        {
+            BodyLines.Add(Line);
+        }
+
+        public IEnumerable<string> Invoke(List<string> Params)
+        {
+            return BodyLines.Select(ln =>
+            {
+                for (var i = 0; i < ParamNames.Count; i++)
+                {
+                    //TODO: whitespace-tokenize the line to prevent clbuttic substitution
+                    ln = ln.Replace(ParamNames[i], Params[i]);
+                }
+                return ln;
+            });
+        }
+    }
+
     public class IC10Program
     {
         public List<ProgramSection> Sections { get; } = [];
+        public Dictionary<string, Macro> Macros { get; } = new();
 
         public HashSet<string> Symbols { get; } = [];
         public HashSet<string> Aliases { get; } = [];
